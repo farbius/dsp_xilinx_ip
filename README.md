662ae6f59053d6150ad15e715dbbd51a548e4032
aleksei <aleksei.rostov@protonmail.com>
2022-03-14 21:17:29 +0100

    md editing

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # dsp_xilinx_ip
 Some basic DSP algorithms implemented with xilinx IP cores with explanation, Verilog testbenches and modelling in Python 
+
+## Software
+
+| Software | Version, plugins |
+| ------ | ------ |
+| CygWin | make, git |
+| Python | Python 3.9.0, Numpy, matplotlib |
+| Vivado | 2019.1 |
---
+++ build/dsp.srcs/sources_1/ip/cic_interpolator/synth/cic_interpolator.vhd
@@ -0,0 +1,210 @@
+-- (c) Copyright 1995-2022 Xilinx, Inc. All rights reserved.
+-- 
+-- This file contains confidential and proprietary information
+-- of Xilinx, Inc. and is protected under U.S. and
+-- international copyright and other intellectual property
+-- laws.
+-- 
+-- DISCLAIMER
+-- This disclaimer is not a license and does not grant any
+-- rights to the materials distributed herewith. Except as
+-- otherwise provided in a valid license issued to you by
+-- Xilinx, and to the maximum extent permitted by applicable
+-- law: (1) THESE MATERIALS ARE MADE AVAILABLE "AS IS" AND
+-- WITH ALL FAULTS, AND XILINX HEREBY DISCLAIMS ALL WARRANTIES
+-- AND CONDITIONS, EXPRESS, IMPLIED, OR STATUTORY, INCLUDING
+-- BUT NOT LIMITED TO WARRANTIES OF MERCHANTABILITY, NON-
+-- INFRINGEMENT, OR FITNESS FOR ANY PARTICULAR PURPOSE; and
+-- (2) Xilinx shall not be liable (whether in contract or tort,
+-- including negligence, or under any other theory of
+-- liability) for any loss or damage of any kind or nature
+-- related to, arising under or in connection with these
+-- materials, including for any direct, or any indirect,
+-- special, incidental, or consequential loss or damage
+-- (including loss of data, profits, goodwill, or any type of
+-- loss or damage suffered as a result of any action brought
+-- by a third party) even if such damage or loss was
+-- reasonably foreseeable or Xilinx had been advised of the
+-- possibility of the same.
+-- 
+-- CRITICAL APPLICATIONS
+-- Xilinx products are not designed or intended to be fail-
+-- safe, or for use in any application requiring fail-safe
+-- performance, such as life-support or safety devices or
+-- systems, Class III medical devices, nuclear facilities,
+-- applications related to the deployment of airbags, or any
+-- other applications that could lead to death, personal
+-- injury, or severe property or environmental damage
+-- (individually and collectively, "Critical
+-- Applications"). Customer assumes the sole risk and
+-- liability of any use of Xilinx products in Critical
+-- Applications, subject only to applicable laws and
+-- regulations governing limitations on product liability.
+-- 
+-- THIS COPYRIGHT NOTICE AND DISCLAIMER MUST BE RETAINED AS
+-- PART OF THIS FILE AT ALL TIMES.
+-- 
+-- DO NOT MODIFY THIS FILE.
+
+-- IP VLNV: xilinx.com:ip:cic_compiler:4.0
+-- IP Revision: 14
+
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+
+LIBRARY cic_compiler_v4_0_14;
+USE cic_compiler_v4_0_14.cic_compiler_v4_0_14;
+
+ENTITY cic_interpolator IS
+  PORT (
+    aclk : IN STD_LOGIC;
+    aresetn : IN STD_LOGIC;
+    s_axis_data_tdata : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+    s_axis_data_tvalid : IN STD_LOGIC;
+    s_axis_data_tready : OUT STD_LOGIC;
+    m_axis_data_tdata : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+    m_axis_data_tvalid : OUT STD_LOGIC
+  );
+END cic_interpolator;
+
+ARCHITECTURE cic_interpolator_arch OF cic_interpolator IS
+  ATTRIBUTE DowngradeIPIdentifiedWarnings : STRING;
+  ATTRIBUTE DowngradeIPIdentifiedWarnings OF cic_interpolator_arch: ARCHITECTURE IS "yes";
+  COMPONENT cic_compiler_v4_0_14 IS
+    GENERIC (
+      C_COMPONENT_NAME : STRING;
+      C_FILTER_TYPE : INTEGER;
+      C_NUM_STAGES : INTEGER;
+      C_DIFF_DELAY : INTEGER;
+      C_RATE : INTEGER;
+      C_INPUT_WIDTH : INTEGER;
+      C_OUTPUT_WIDTH : INTEGER;
+      C_USE_DSP : INTEGER;
+      C_HAS_ROUNDING : INTEGER;
+      C_NUM_CHANNELS : INTEGER;
+      C_RATE_TYPE : INTEGER;
+      C_MIN_RATE : INTEGER;
+      C_MAX_RATE : INTEGER;
+      C_SAMPLE_FREQ : INTEGER;
+      C_CLK_FREQ : INTEGER;
+      C_USE_STREAMING_INTERFACE : INTEGER;
+      C_FAMILY : STRING;
+      C_XDEVICEFAMILY : STRING;
+      C_C1 : INTEGER;
+      C_C2 : INTEGER;
+      C_C3 : INTEGER;
+      C_C4 : INTEGER;
+      C_C5 : INTEGER;
+      C_C6 : INTEGER;
+      C_I1 : INTEGER;
+      C_I2 : INTEGER;
+      C_I3 : INTEGER;
+      C_I4 : INTEGER;
+      C_I5 : INTEGER;
+      C_I6 : INTEGER;
+      C_S_AXIS_CONFIG_TDATA_WIDTH : INTEGER;
+      C_S_AXIS_DATA_TDATA_WIDTH : INTEGER;
+      C_M_AXIS_DATA_TDATA_WIDTH : INTEGER;
+      C_M_AXIS_DATA_TUSER_WIDTH : INTEGER;
+      C_HAS_DOUT_TREADY : INTEGER;
+      C_HAS_ACLKEN : INTEGER;
+      C_HAS_ARESETN : INTEGER
+    );
+    PORT (
+      aclk : IN STD_LOGIC;
+      aclken : IN STD_LOGIC;
+      aresetn : IN STD_LOGIC;
+      s_axis_config_tdata : IN STD_LOGIC_VECTOR(0 DOWNTO 0);
+      s_axis_config_tvalid : IN STD_LOGIC;
+      s_axis_config_tready : OUT STD_LOGIC;
+      s_axis_data_tdata : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+      s_axis_data_tvalid : IN STD_LOGIC;
+      s_axis_data_tready : OUT STD_LOGIC;
+      s_axis_data_tlast : IN STD_LOGIC;
+      m_axis_data_tdata : OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+      m_axis_data_tuser : OUT STD_LOGIC_VECTOR(0 DOWNTO 0);
+      m_axis_data_tvalid : OUT STD_LOGIC;
+      m_axis_data_tready : IN STD_LOGIC;
+      m_axis_data_tlast : OUT STD_LOGIC;
+      event_tlast_unexpected : OUT STD_LOGIC;
+      event_tlast_missing : OUT STD_LOGIC;
+      event_halted : OUT STD_LOGIC
+    );
+  END COMPONENT cic_compiler_v4_0_14;
+  ATTRIBUTE X_CORE_INFO : STRING;
+  ATTRIBUTE X_CORE_INFO OF cic_interpolator_arch: ARCHITECTURE IS "cic_compiler_v4_0_14,Vivado 2019.1";
+  ATTRIBUTE CHECK_LICENSE_TYPE : STRING;
+  ATTRIBUTE CHECK_LICENSE_TYPE OF cic_interpolator_arch : ARCHITECTURE IS "cic_interpolator,cic_compiler_v4_0_14,{}";
+  ATTRIBUTE CORE_GENERATION_INFO : STRING;
+  ATTRIBUTE CORE_GENERATION_INFO OF cic_interpolator_arch: ARCHITECTURE IS "cic_interpolator,cic_compiler_v4_0_14,{x_ipProduct=Vivado 2019.1,x_ipVendor=xilinx.com,x_ipLibrary=ip,x_ipName=cic_compiler,x_ipVersion=4.0,x_ipCoreRevision=14,x_ipLanguage=VERILOG,x_ipSimLanguage=MIXED,C_COMPONENT_NAME=cic_interpolator,C_FILTER_TYPE=0,C_NUM_STAGES=6,C_DIFF_DELAY=1,C_RATE=4,C_INPUT_WIDTH=16,C_OUTPUT_WIDTH=26,C_USE_DSP=1,C_HAS_ROUNDING=0,C_NUM_CHANNELS=1,C_RATE_TYPE=0,C_MIN_RATE=4,C_MAX_RATE=4,C_SAMPLE_FREQ=1,C_CLK_FREQ=4,C_USE_STREAMING_INTERFACE=1,C_FAMILY=zynquplus,C_XDEVICEFA" & 
+"MILY=zynquplus,C_C1=17,C_C2=18,C_C3=19,C_C4=20,C_C5=21,C_C6=21,C_I1=21,C_I2=22,C_I3=23,C_I4=24,C_I5=25,C_I6=26,C_S_AXIS_CONFIG_TDATA_WIDTH=1,C_S_AXIS_DATA_TDATA_WIDTH=16,C_M_AXIS_DATA_TDATA_WIDTH=32,C_M_AXIS_DATA_TUSER_WIDTH=1,C_HAS_DOUT_TREADY=0,C_HAS_ACLKEN=0,C_HAS_ARESETN=1}";
+  ATTRIBUTE X_INTERFACE_INFO : STRING;
+  ATTRIBUTE X_INTERFACE_PARAMETER : STRING;
+  ATTRIBUTE X_INTERFACE_INFO OF m_axis_data_tvalid: SIGNAL IS "xilinx.com:interface:axis:1.0 M_AXIS_DATA TVALID";
+  ATTRIBUTE X_INTERFACE_PARAMETER OF m_axis_data_tdata: SIGNAL IS "XIL_INTERFACENAME M_AXIS_DATA, TDATA_NUM_BYTES 4, TDEST_WIDTH 0, TID_WIDTH 0, TUSER_WIDTH 0, HAS_TREADY 0, HAS_TSTRB 0, HAS_TKEEP 0, HAS_TLAST 0, FREQ_HZ 100000000, PHASE 0.000, LAYERED_METADATA undef, INSERT_VIP 0";
+  ATTRIBUTE X_INTERFACE_INFO OF m_axis_data_tdata: SIGNAL IS "xilinx.com:interface:axis:1.0 M_AXIS_DATA TDATA";
+  ATTRIBUTE X_INTERFACE_INFO OF s_axis_data_tready: SIGNAL IS "xilinx.com:interface:axis:1.0 S_AXIS_DATA TREADY";
+  ATTRIBUTE X_INTERFACE_INFO OF s_axis_data_tvalid: SIGNAL IS "xilinx.com:interface:axis:1.0 S_AXIS_DATA TVALID";
+  ATTRIBUTE X_INTERFACE_PARAMETER OF s_axis_data_tdata: SIGNAL IS "XIL_INTERFACENAME S_AXIS_DATA, TDATA_NUM_BYTES 2, TDEST_WIDTH 0, TID_WIDTH 0, TUSER_WIDTH 0, HAS_TREADY 1, HAS_TSTRB 0, HAS_TKEEP 0, HAS_TLAST 0, FREQ_HZ 100000000, PHASE 0.000, LAYERED_METADATA undef, INSERT_VIP 0";
+  ATTRIBUTE X_INTERFACE_INFO OF s_axis_data_tdata: SIGNAL IS "xilinx.com:interface:axis:1.0 S_AXIS_DATA TDATA";
+  ATTRIBUTE X_INTERFACE_PARAMETER OF aresetn: SIGNAL IS "XIL_INTERFACENAME aresetn_intf, POLARITY ACTIVE_LOW, INSERT_VIP 0";
+  ATTRIBUTE X_INTERFACE_INFO OF aresetn: SIGNAL IS "xilinx.com:signal:reset:1.0 aresetn_intf RST";
+  ATTRIBUTE X_INTERFACE_PARAMETER OF aclk: SIGNAL IS "XIL_INTERFACENAME aclk_intf, ASSOCIATED_BUSIF S_AXIS_CONFIG:M_AXIS_DATA:S_AXIS_DATA, ASSOCIATED_RESET aresetn, ASSOCIATED_CLKEN aclken, FREQ_HZ 100000000, PHASE 0.000, INSERT_VIP 0";
+  ATTRIBUTE X_INTERFACE_INFO OF aclk: SIGNAL IS "xilinx.com:signal:clock:1.0 aclk_intf CLK";
+BEGIN
+  U0 : cic_compiler_v4_0_14
+    GENERIC MAP (
+      C_COMPONENT_NAME => "cic_interpolator",
+      C_FILTER_TYPE => 0,
+      C_NUM_STAGES => 6,
+      C_DIFF_DELAY => 1,
+      C_RATE => 4,
+      C_INPUT_WIDTH => 16,
+      C_OUTPUT_WIDTH => 26,
+      C_USE_DSP => 1,
+      C_HAS_ROUNDING => 0,
+      C_NUM_CHANNELS => 1,
+      C_RATE_TYPE => 0,
+      C_MIN_RATE => 4,
+      C_MAX_RATE => 4,
+      C_SAMPLE_FREQ => 1,
+      C_CLK_FREQ => 4,
+      C_USE_STREAMING_INTERFACE => 1,
+      C_FAMILY => "zynquplus",
+      C_XDEVICEFAMILY => "zynquplus",
+      C_C1 => 17,
+      C_C2 => 18,
+      C_C3 => 19,
+      C_C4 => 20,
+      C_C5 => 21,
+      C_C6 => 21,
+      C_I1 => 21,
+      C_I2 => 22,
+      C_I3 => 23,
+      C_I4 => 24,
+      C_I5 => 25,
+      C_I6 => 26,
+      C_S_AXIS_CONFIG_TDATA_WIDTH => 1,
+      C_S_AXIS_DATA_TDATA_WIDTH => 16,
+      C_M_AXIS_DATA_TDATA_WIDTH => 32,
+      C_M_AXIS_DATA_TUSER_WIDTH => 1,
+      C_HAS_DOUT_TREADY => 0,
+      C_HAS_ACLKEN => 0,
+      C_HAS_ARESETN => 1
+    )
+    PORT MAP (
+      aclk => aclk,
+      aclken => '1',
+      aresetn => aresetn,
+      s_axis_config_tdata => STD_LOGIC_VECTOR(TO_UNSIGNED(0, 1)),
+      s_axis_config_tvalid => '0',
+      s_axis_data_tdata => s_axis_data_tdata,
+      s_axis_data_tvalid => s_axis_data_tvalid,
+      s_axis_data_tready => s_axis_data_tready,
+      s_axis_data_tlast => '0',
+      m_axis_data_tdata => m_axis_data_tdata,
+      m_axis_data_tvalid => m_axis_data_tvalid,
+      m_axis_data_tready => '0'
+    );
+END cic_interpolator_arch;
